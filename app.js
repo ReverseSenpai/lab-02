26e559a438320d4f2ff12d6bb17e2eb4b528db52
Anthony Aries <anthony.aries99@gmail.com>
2017-04-04 13:20:49 -0700

    intial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var okonomiyaki = prompt('Do you think i like okonomiyaki?');
+if (okonomiyaki.trim().toLowerCase() == 'yes'){
+  alert('you were correct');
+}else{
+  alert('you know nothing');
+}
+var league = prompt('Do you think I love video games?');
+if (league.trim().toLowerCase() == 'yes'){
+  alert('you were correct');
+}else{
+  alert('you know nothing');
+}
