02e3aa24c43d8a87facc2cd379b84369cb2b7b9d
Anthony Aries <anthony.aries99@gmail.com>
2017-04-04 13:55:35 -0700

    finished reqs for assign

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 
 var okonomiyaki = prompt('Do you think i like okonomiyaki?');
 if (okonomiyaki.trim().toLowerCase() == 'yes'){
-  alert('you were correct');
+  alert('You are correct');
 }else{
-  alert('you know nothing');
+  alert('You know nothing');
 }
 var league = prompt('Do you think I love video games?');
 if (league.trim().toLowerCase() == 'yes'){
-  alert('you were correct');
+  alert('You are correct');
 }else{
-  alert('you know nothing');
+  alert('You know nothing');
+}
+var bone = prompt('Have i ever broken a bone?');
+if (bone.trim().toLowerCase() == 'no'){
+  alert('You are correct');
+} else {
+  alert('You know nothing');
+}
+var anime = prompt('Do i watch anime?');
+if (anime.trim().toLowerCase() == 'yes'){
+  alert('You are correct');
+}else{
+  alert('You know nothing');
+}
+var height= prompt('Do you think im at least 6 feet tall?');
+if (height.trim().toLowerCase() == 'yes'){
+  alert ('You are correct');
+}else{
+  alert('You know nothing')
 }
