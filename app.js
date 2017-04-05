276dbf0e7b66699355e8a36ff46286688bdc76e0
Anthony Aries <anthony.aries99@gmail.com>
2017-04-05 14:14:51 -0700

    added 6th q

--- app.js
+++ app.js
@@ -1,42 +1,121 @@
-'use strict';
+ 'use strict';
 
-var okonomiyaki = prompt('Do you think i like okonomiyaki?');
-if (okonomiyaki.trim().toLowerCase() == 'yes'){
-  alert('You are correct');
-}else{
-  alert('You know nothing');
+var correctNumber =13;
+var notDone = true;
+
+var guess;
+for (var guesses=4; guesses > 0 && notDone; guesses--) {
+  guess = prompt('Can you guess my favorite number? You get 4 chances');
+  guess = parseInt(guess);
+  if (guess === correctNumber) {
+    notDone = false;
+    alert('Correct!');
+  } else if (guesses === 1 ){
+    alert ('you are out of guesses')
+  } else if (guess > 13){
+    alert ('too high guess again');
+  } else if (guess < 13){
+    alert ('too low guess again');
+  }
+
 }
-console.log(okonomiyaki);
-var league = prompt('Do you think I love video games?');
-if (league.trim().toLowerCase() == 'yes'){
-  alert('You are correct');
-}else{
-  alert('You know nothing');
-}
-console.log(league);
-var bone = prompt('Have i ever broken a bone?');
-if (bone.trim().toLowerCase() == 'no'){
-  alert('You are correct');
-} else {
-  alert('You know nothing');
-}
-console.log(bone);
-var anime = prompt('Do i watch anime?');
-if (anime.trim().toLowerCase() == 'yes'){
-  alert('You are correct');
-}else{
-  alert('You know nothing');
-}
-console.log(anime);
-var height= prompt('Do you think im at least 6 feet tall?');
-if (height.trim().toLowerCase() == 'yes'){
-  alert ('You are correct');
-}else{
-  alert('You know nothing')
-}
-console.log(height);
-if (okonomiyaki== 'yes' &league=='yes'&bone=='no'&anime=='yes'&height=='yes'){
-  alert('Wow you really know me!');
-}else{
-  alert('Since you dont know that much about me heres some info :D');
-}
+// var videoGames = ['Kingdom Hearts', 'League of Legends', 'Dark Souls', 'Fallout New Vegas']
+// var guess;
+// for (guesses=6; guesses > 0 && notDone; guesses--) {
+//   guess = prompt('Guess some of my favorite videogames. 6 chances this time?');
+//   if (guess === 'Kingdom Hearts' ) {
+//     notDone = false;
+//     alert('Correct!');
+//   }
+// var okonomiyaki = prompt('Do you think i like okonomiyaki?');
+// if (okonomiyaki.trim().toLowerCase() == 'yes'){
+//   alert('You are correct');
+// }else{
+//   alert('You know nothing');
+// }
+// console.log(okonomiyaki);
+// var league = prompt('Do you think I love video games?');
+// if (league.trim().toLowerCase() == 'yes'){
+//   alert('You are correct');
+// }else{
+//   alert('You know nothing');
+// }
+// console.log(league);
+// var bone = prompt('Have i ever broken a bone?');
+// if (bone.trim().toLowerCase() == 'no'){
+//   alert('You are correct');
+// } else {
+//   alert('You know nothing');
+// }
+// console.log(bone);
+// var anime = prompt('Do i watch anime?');
+// if (anime.trim().toLowerCase() == 'yes'){
+//   alert('You are correct');
+// }else{
+//   alert('You know nothing');
+// }
+// console.log(anime);
+// var height= prompt('Do you think im at least 6 feet tall?');
+// if (height.trim().toLowerCase() == 'yes'){
+//   alert ('You are correct');
+// }else{
+//   alert('You know nothing')
+// }
+// console.log(height);
+// if (okonomiyaki== 'yes' &league=='yes'&bone=='no'&anime=='yes'&height=='yes'){
+//   alert('Wow you really know me!');
+// }else{
+//   alert('Since you dont know that much about me heres some info :D');
+// }
+// class three code demo
+
+
+//DATA TYPES IN JS
+//primitives
+// String
+// Number
+// Boolean
+//
+// Object
+// Function
+// Array
+// null
+// undefined
+
+// var slug =120;
+//  slug=slug * 2;
+//  slug -3;
+//  slug /2;
+//  slug % 4;
+//
+//  slug+=100
+// slug = Math.pow(slug,4)
+// Math.floor(234.34); //234
+// Math.ceil(234.34); //235
+// Math.round(234.49); //234
+// Math.round(234.5); //235
+//
+// var happy = true; //true
+// happy= !happy; //false
+
+// null // a programmare chose for the vaule to be  nothing
+// undefined // litteraly means nothing has been defined
+/*
+\n //return (new line car)
+\t //tab
+\\ //actual
+\" //actual double quote
+\'actualy single quote
+*/
+// message= message + '\n go to the market'
+
+
+//rogue 1;
+
+// if (condition){
+//   run this chunk of code
+// } else if {
+//   run this other chunk of code
+// } else {
+//   run this final chunk of code
+// }
